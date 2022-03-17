a9b55b542570c68fa047a198174ddae81d51dfc6
LovelyMoon23 <moonsirl9123@gmail.com>
2022-03-17 12:26:29 +0900

    반응형웹 기초강의 정리

---
+++ 반응형웹/3.뷰포트단위.js
@@ -0,0 +1,12 @@
+// em과 rem: 반응하진 않아요
+
+// em 과 rem은 주변 상황에 따라 그 크기를 달리할 수 있는 가변성을 지니고는 있지만
+// 브라우저나 기기 화면에 크기에 따라 달라지는 크기는 아니다!
+// 따라서 진정한 반응형 단위라 할 수 없다
+
+// 반응하는 단위들
+// font-size: 1vw 뷰포트의 width 100분의 1
+// font-size: 1vh 뷰포트의 height 100분의 1
+
+// font-size: 1vmin 뷰포트의 width와 height중 작은 쪽의 100분의 1
+// font-size:1vmax  뷰포트의 width와 height중 큰 쪽의 100분의 1
